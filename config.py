580693f4c680a76177d1bb8d749d4373edbf0593
hTmlDP <html@htmldp.com>
2015-02-27 18:30:41 +0100

    Add

---
+++ config.py
@@ -0,0 +1,3 @@
+API_KEY="" #Your http://po.st API key, you can get it for FREE at http://re.po.st
+WHITELIST=['www.youtube.com/watch?v='] #Don't ask to shrink these url's
+BLACKLIST=['www.google', 'facebook', 'www.facebook'] #Don't shrink matching url's
